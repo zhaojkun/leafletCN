375d8c34e10a67474aef2d852826546769f0b9c0
Zhao Jiangkun <zhaojk90@gmail.com>
2021-09-01 00:42:39 +0800

    change to en

--- R/Amap.R
+++ R/Amap.R
@@ -19,7 +19,7 @@ amap = function(map,
                 attribution = '&copy; <a href="http://amap.com">amap.com</a >',
                 ...){
   leaflet::addTiles(map,
-           'http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}',
+           'http://webrd02.is.autonavi.com/appmaptile?lang=en&size=1&scale=1&style=8&x={x}&y={y}&z={z}',
            leaflet::tileOptions(tileSize=256,  minZoom=3,maxZoom=17),
            attribution = attribution,
            ...)
